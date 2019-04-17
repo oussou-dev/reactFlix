dba666940d4a4b0037aef4a5a09eab67d16e5369
oussou-dev <oussou.dev@gmail.com>
2019-04-17 10:59:46 +0200

    Adding Components + Containers Folders

---
+++ src/components/VideoListItem.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class VideoListItem extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello CodeSandbox</h1>
+				<h2>Start editing to see some magic happen!</h2>
+			</div>
+		)
+	}
+}
+
+export default VideoListItem
